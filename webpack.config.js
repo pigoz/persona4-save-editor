90068107ea6ea89f12832c65c296cb9460b7c109
Stefano Pigozzi <stefano.pigozzi@gmail.com>
2017-11-03 09:58:08 +0100

    run uglify only in production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
 const buildDir = path.join(__dirname, "build");
 const src = file => path.join(__dirname, './src', file);
+
+function uglify(xs) {
+  if (process.env.NODE_ENV === 'production') {
+    xs.push(new UglifyJSPlugin({
+      parallel: true,
+      uglifyOptions: {
+        ie8: false,
+        ecma: 8,
+        output: {
+          comments: false,
+          beautify: false,
+        },
+      },
+    }));
+  }
+  return xs;
+}
 
 module.exports = {
   entry: src('index.re'),
@@ -11,18 +30,26 @@ module.exports = {
   },
   module: {
     rules: [
-      { test: /\.(re|ml)$/, use: 'bs-loader' },
+      {
+        test: /\.(re|ml)$/,
+        use: {
+          loader: 'bs-loader',
+          options: {
+            module: 'es6',
+          },
+        },
+      },
     ],
   },
   resolve: {
     extensions: ['.re', '.ml', '.js'],
   },
-  plugins: [
+  plugins: uglify([
     new HtmlWebpackPlugin({
       inject: true,
       template: src('index.html'),
     }),
-  ],
+  ]),
   devServer: {
     contentBase: buildDir,
     compress: true,
